1ca2ad36384b0d5ef37f0d5088c8c3c026610b03
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2020-10-27 20:25:00 +0900

    Implement "Money" model

--- src/money/Money.spec.ts
+++ src/money/Money.spec.ts
@@ -1,1 +1,121 @@
-test.todo('Money Model の動作確認');
+import { Money } from './Money';
+
+describe('abs', () => {
+  test('+ to +', () => {
+    const money = Money.create(100, 'JPY');
+
+    expect(Money.abs(money).amount).toEqual(money.amount);
+  });
+
+  test('- to +', () => {
+    const money = Money.create(-100, 'JPY');
+
+    expect(Money.abs(money).amount).toEqual(-money.amount);
+  });
+});
+
+describe('add', () => {
+  test('same currency', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'JPY');
+
+    expect(Money.add(lhs, rhs).amount).toEqual(lhs.amount + rhs.amount);
+  });
+
+  test('different currency', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'USD');
+
+    expect(() => Money.add(lhs, rhs).amount).toThrow();
+  });
+});
+
+describe('compare', () => {
+  test('lhs === rhs', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'JPY');
+
+    expect(Money.compare(lhs, rhs)).toEqual(0);
+  });
+
+  test('lhs < rhs', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(200, 'JPY');
+
+    expect(Money.compare(lhs, rhs)).toEqual(-1);
+  });
+
+  test('lhs > rhs', () => {
+    const lhs = Money.create(200, 'JPY');
+    const rhs = Money.create(100, 'JPY');
+
+    expect(Money.compare(lhs, rhs)).toEqual(1);
+  });
+
+  test('different currency', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'USD');
+
+    expect(() => Money.compare(lhs, rhs)).toThrow();
+  });
+});
+
+describe('equals', () => {
+  test('same currency, same amount', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'JPY');
+
+    expect(Money.equals(lhs, rhs)).toEqual(true);
+  });
+
+  test('same currency, different amount', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(200, 'JPY');
+
+    expect(Money.equals(lhs, rhs)).toEqual(false);
+  });
+
+  test('different currency, same amount', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'USD');
+
+    expect(Money.equals(lhs, rhs)).toEqual(false);
+  });
+
+  test('different currency, different amount', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(200, 'USD');
+
+    expect(Money.equals(lhs, rhs)).toEqual(false);
+  });
+});
+
+describe('negated', () => {
+  test('+ to -', () => {
+    const money = Money.create(100, 'JPY');
+
+    expect(Money.negated(money).amount).toEqual(-money.amount);
+  });
+
+  test('- to +', () => {
+    const money = Money.create(-100, 'JPY');
+
+    expect(Money.negated(money).amount).toEqual(-money.amount);
+  });
+});
+
+describe('subtract', () => {
+  test('same currency', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'JPY');
+
+    expect(Money.subtract(lhs, rhs).amount).toEqual(lhs.amount - rhs.amount);
+  });
+
+  test('different currency', () => {
+    const lhs = Money.create(100, 'JPY');
+    const rhs = Money.create(100, 'USD');
+
+    expect(() => Money.subtract(lhs, rhs).amount).toThrow();
+  });
+});
---
+++ src/money/Money.ts
@@ -0,0 +1,111 @@
+type Currency = 'JPY' | 'USD' | 'EUR';
+
+export interface Money {
+  readonly _tag: 'Money';
+  readonly amount: number;
+  readonly currency: Currency;
+}
+
+function create(amount: number, currency: Currency): Money {
+  return {
+    _tag: 'Money',
+    amount,
+    currency,
+  };
+}
+
+function equals(lhs: Money, rhs: Money): boolean {
+  return lhs.amount === rhs.amount && lhs.currency === rhs.currency;
+}
+
+function abs(money: Money): Money {
+  return {
+    ...money,
+    amount: Math.abs(money.amount),
+  };
+}
+
+function negated(money: Money): Money {
+  return {
+    ...money,
+    amount: -money.amount,
+  };
+}
+
+function compare(lhs: Money, rhs: Money): -1 | 0 | 1 {
+  if (lhs.currency !== rhs.currency) throw new Error('IllegalArgumentError');
+
+  if (lhs.amount < rhs.amount) return -1;
+  if (lhs.amount > rhs.amount) return 1;
+
+  return 0;
+}
+
+function dividedBy(money: Money, divisor: number): Money {
+  return {
+    ...money,
+    amount: money.amount / divisor,
+  };
+}
+
+function times(money: Money, other: number): Money {
+  return {
+    ...money,
+    amount: money.amount * other,
+  };
+}
+
+function add(lhs: Money, rhs: Money): Money {
+  if (lhs.currency !== rhs.currency) throw new Error('IllegalArgumentError');
+
+  return {
+    ...lhs,
+    amount: lhs.amount + rhs.amount,
+  };
+}
+
+function subtract(lhs: Money, rhs: Money): Money {
+  if (lhs.currency !== rhs.currency) throw new Error('IllegalArgumentError');
+
+  return {
+    ...lhs,
+    amount: lhs.amount - rhs.amount,
+  };
+}
+
+function isGreaterThan(lhs: Money, rhs: Money): boolean {
+  return compare(lhs, rhs) === 1;
+}
+
+function isLessThan(lhs: Money, rhs: Money): boolean {
+  return compare(lhs, rhs) === -1;
+}
+
+function isPositive(money: Money): boolean {
+  return compare(money, create(0, money.currency)) === 1;
+}
+
+function isNegative(money: Money): boolean {
+  return compare(money, create(0, money.currency)) === -1;
+}
+
+function isZero(money: Money): boolean {
+  return compare(money, create(0, money.currency)) === 0;
+}
+
+export const Money = {
+  abs,
+  add,
+  compare,
+  create,
+  dividedBy,
+  equals,
+  isGreaterThan,
+  isLessThan,
+  isNegative,
+  isPositive,
+  isZero,
+  negated,
+  subtract,
+  times,
+};
